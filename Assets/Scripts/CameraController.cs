91e9584fd05b3a4b023f3ce89491bd11d37ef40d
russ-stirling <rstirli2@uwo.ca>
2017-09-15 20:58:10 -0400

    Unity basic tutorial for roll a ball game.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject Player;
+
+    private Vector3 Offset;
+
+	// Use this for initialization
+	void Start () {
+        Offset = transform.position - Player.transform.position;
+	}
+	
+	// Late Update is called once per frame but guarenteed to run after all items in update processed so we know player moved
+	void LateUpdate () {
+        transform.position = Player.transform.position + Offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speedMultipier;
+    private Rigidbody rb;
+    private int Score;
+    public Text ScoreText;
+    public Text VictoryText;
+
+    void Start()
+    {
+        Score = 0;
+        rb = GetComponent<Rigidbody>();
+        SetTextOfScore();
+        VictoryText.text = string.Empty;
+    }
+    void FixedUpdate()
+    {
+        float moveHoriz = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHoriz, 0, moveVert);
+
+        rb.AddForce(movement * speedMultipier);
+    }
+
+    private void SetTextOfScore()
+    {
+        ScoreText.text = string.Format("Score: {0}", Score);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            Score++;
+            SetTextOfScore();
+            if(Score>=6)
+            {
+                VictoryText.text = "Congratulations!!";
+            }
+        }
+
+        
+        //Destroy(other.gameObject);
+    }
+}
